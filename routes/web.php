8f8c01ffa3a754c351c4a777fb4784eb4cf710d6
Brandon Isahir Hernández Camacho <brandonhecam@gmail.com>
2023-12-20 14:50:25 -0600

    refactor(rutas): mejorar la estructura de las rutas del proyecto

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
-
-Route::get('blog', function () {
-    // Consulta a la base de datos
-    $posts = [
-        [
-            'id' => 1,
-            'title' => 'Primer post',
-            'slug' => 'primer-post'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Segundo post',
-            'slug' => 'segundo-post'
-        ],
-    ];
-    return view('blog', compact('posts'));
-})->name('blog');
-
-Route::get('blog/{slug}', function ($slug) {
-    // Consulta a la base de datos
-    $post = $slug;
-    return view('post', ['post' => $post]);
-})->name('blog-post');
+Route::controller(PageController::class)->group(function () {
+    Route::get('/', 'home')->name('home');
+    Route::get('blog', 'blog')->name('blog');
+    Route::get('blog/{slug}', 'blogPost')->name('blog-post');
+});
