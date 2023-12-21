b469243d4244098f1c4beb4dd4dd2c0d5b7f804f
Brandon Isahir Hernández Camacho <brandonhecam@gmail.com>
2023-12-20 18:52:42 -0600

    feat(migración): agregar llave foránea a la tabla posts

--- database/migrations/2023_12_20_213457_create_posts_table.php
+++ database/migrations/2023_12_20_213457_create_posts_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->references('id')->on('users');
             $table->string('title');
             $table->string('slug')->unique();
             $table->text('body');
